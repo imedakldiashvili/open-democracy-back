f25de6ab4e08ea114d464e90b537cb763ff92834
imedakldiasvhvili <imeda.kldiashvili@gmail.com>
2022-12-24 00:48:31 +0400

    FB Google Button

--- src/utils/otpCode.ts
+++ src/utils/otpCode.ts
@@ -1,4 +1,6 @@
+import appSettings from '../settings'
 export const otpCode = () => { 
-  const random = Math.floor(Math.random() * 9000 + 1000);
+  
+  const random = appSettings.OTP_CODE == "random" ? Math.floor(Math.random() * 9000 + 1000) : 1111;
   return random
 } 
