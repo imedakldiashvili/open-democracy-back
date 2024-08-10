46c1bcd209211bf2c19899fe51fe91c32394c6d3
imedakldiasvhvili <imeda.kldiashvili@gmail.com>
2024-08-11 03:32:05 +0400

    Fix - tbcAccountMovements

--- src/modules/banks/api/tbc/TBCApi.ts
+++ src/modules/banks/api/tbc/TBCApi.ts
@@ -51,14 +51,15 @@ export const tbcAccountMovements = async () => {
     const bankSettings = await servicebankSettings();
     const bankSettingTbcAccount = bankSettings.filter(e => e.code == "TBC_ACCOUNT")[0]
     const bankSettingTbcUserName = bankSettings.filter(e => e.code == "TBC_USER_NAME")[0]
+    const bankSettingTbcMovementStart = bankSettings.filter(e => e.code == "TBS_MOVEMENT_DATE_START")[0]
 
     const tbcDigipass = 'notMandatory'
     const account = bankSettingTbcAccount.value
     const tbcUsername = bankSettingTbcUserName.value
+    const statDate = bankSettingTbcMovementStart.value
     const tbcActualPassword = await tbcGetActualPassword()
-    const statDate = '2015-01-01T00:00:00.000'
     const pageSize = 700
-    
+
     let pageIndex = 0
     let totalNumber = pageSize
 
@@ -73,10 +74,7 @@ export const tbcAccountMovements = async () => {
 
     const instance = getAxiosInstance();
 
-     
-    while (totalNumber >= pageIndex * pageSize)
-    {
-            
+    while (true) {
       const xmlReqBody = accountMovementsXmlReqBody(tbcUsername, tbcActualPassword, tbcDigipass, statDate, pageIndex.toString(), pageSize.toString())
       const config = accountMovementsConfig()
       const response = await instance.post(baseUrl, xmlReqBody, config);
@@ -86,50 +84,29 @@ export const tbcAccountMovements = async () => {
 
       const pageResult = parserResultObj["SOAP-ENV:Envelope"]["SOAP-ENV:Body"]["ns2:GetAccountMovementsResponseIo"]["ns2:result"]
       const resultTotalNumber = pageResult["ns2:totalCount"];
-
-      const transactionsResult = parserResultObj["SOAP-ENV:Envelope"]["SOAP-ENV:Body"]["ns2:GetAccountMovementsResponseIo"]["ns2:accountMovement"]
-
       totalNumber = resultTotalNumber
 
-      for (var t of transactionsResult) {
-        const debitCredit = t["ns2:debitCredit"]
-        if (!debitCredit) { continue }
-
-        const accountNumber = t["ns2:accountNumber"]
-        if (account != accountNumber) { continue }
-        
-        const partnerAccountNumber = t["ns2:partnerAccountNumber"]
-        const currency = t["ns2:amount"]["ns2:currency"]
-        const externalPaymentId = t["ns2:externalPaymentId"]
-        const valueDate = t["ns2:valueDate"]
-        const partnerTaxCode = t["ns2:partnerTaxCode"]
-        const partnerName = t["ns2:partnerName"]
-        const description = t["ns2:description"]
-        const amount = t["ns2:amount"]["ns2:amount"]
-
-        const transaction = {
-          uid: externalPaymentId,
-          date: valueDate,
-          clientCode: partnerTaxCode,
-          clientName: partnerName,
-          accountNumber: partnerAccountNumber,
-          amount: amount,
-          currency: currency,
-          desctiption: description,
+      const transactionsResult = parserResultObj["SOAP-ENV:Envelope"]["SOAP-ENV:Body"]["ns2:GetAccountMovementsResponseIo"]["ns2:accountMovement"]
+      if (transactionsResult) {
+        if (transactionsResult.length)
+        {
+          for (var t of transactionsResult) {
+            const transaction = getTransaction(t, account)
+            if (transaction) { result.transactions.push(transaction) } 
+          }
         }
-
-        result.transactions.push(transaction)
+        else
+        {
+          const transaction = getTransaction(transactionsResult, account)
+          if (transaction) { result.transactions.push(transaction) }          
+        }
       }
 
+      if (totalNumber < pageIndex * pageSize) { break }
       pageIndex++
 
-      // console.log(totalNumber, pageIndex, pageSize, pageIndex * pageSize)
-
     }
-
     result.paging.totalNumber = totalNumber
-
-    
 
     return result
   } catch (e) {
@@ -143,9 +120,40 @@ export const tbcGetActualPassword = async () => {
   const dateTimeNow = dateNow();
   const allTockens = await bankTokenRepository.find()
   const tbcTokens = allTockens.filter(e => e.bank == bank);
-  const activeTokens = tbcTokens.filter(e => dateTimeNow < e.expiredOn  );
+  const activeTokens = tbcTokens.filter(e => dateTimeNow < e.expiredOn);
   if (activeTokens.length != 1) { throwBadRequest("tbc_actual_password_not_found") }
   const tbcToken = activeTokens[0]
   const actualPassword = tbcToken.token
   return actualPassword
 };
+
+const getTransaction = (xmlTransaction, account) => {
+          const t = xmlTransaction
+          const debitCredit = t["ns2:debitCredit"]
+          if (!debitCredit) { return null }
+
+          const accountNumber = t["ns2:accountNumber"]
+          if (account != accountNumber) { return null }
+
+          const partnerAccountNumber = t["ns2:partnerAccountNumber"]
+          const currency = t["ns2:amount"]["ns2:currency"]
+          const externalPaymentId = t["ns2:externalPaymentId"]
+          const valueDate = t["ns2:valueDate"]
+          const partnerTaxCode = t["ns2:partnerTaxCode"]
+          const partnerName = t["ns2:partnerName"]
+          const description = t["ns2:description"]
+          const amount = t["ns2:amount"]["ns2:amount"]
+
+          const transaction = {
+            uid: externalPaymentId,
+            date: valueDate,
+            clientCode: partnerTaxCode,
+            clientName: partnerName,
+            accountNumber: partnerAccountNumber,
+            amount: amount,
+            currency: currency,
+            desctiption: description,
+          }
+          return transaction
+
+}
