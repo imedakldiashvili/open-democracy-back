fc819ebb0f95d09c37cdb96aa7b0f2ecc09a9c99
imedakldiasvhvili <imeda.kldiashvili@gmail.com>
2025-03-04 17:10:15 +0400

    Fix - addUserInivitation

--- src/modules/users/services/index.ts
+++ src/modules/users/services/index.ts
@@ -267,7 +267,11 @@ export const addUserInivitation = async (mobileNumber: string, fullName: string,
     
     await serviceAddUserInivitaionAction({ sessionUid, inivitaitaionId, createdUserId, mobileNumber, fullName, email })
 
-    const smsText = "welcome to primaries.ge: " + fullName + "\n message sent by " + senderUser.userDetail.fullName;            
+    const smsText = "welcome to primaries.ge\n" 
+                    + fullName 
+                    + "\n" + email 
+                    + "\n" 
+                    + "message sent by " + senderUser.userDetail.fullName;            
     if (mobileNumber) { await sendSMS(mobileNumber, smsText) } 
 
 
