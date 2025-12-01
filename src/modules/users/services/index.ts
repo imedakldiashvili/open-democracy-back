7da6e6a66f49fe797718e0b772a7585555b97dac
imedakldiasvhvili <imeda.kldiashvili@gmail.com>
2025-12-01 20:04:28 +0400

    fix = addOTP

--- src/modules/users/services/index.ts
+++ src/modules/users/services/index.ts
@@ -158,7 +158,7 @@ export const addOTP = async (target: string, deviceUid: string, type: string, va
     otp.expirationDate = dateNowAddMinutes(5)
 
     await userOTPRepository.save(otp)
-    const result = { id: otp.id, type: otp.type, value: otp.value, status: "otp_send_successfuly" }
+    const result = { id: otp.id, code: otp.code, type: otp.type, value: otp.value, status: "otp_send_successfuly" }
     return result
 }
 
